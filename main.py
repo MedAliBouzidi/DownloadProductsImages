fcd4e981f9537232cddd051bbefa5ec276a2d86e
BOUZIDI Med Ali <med23908@gmail.com>
2022-11-08 16:24:55 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import json
+import os
+import ssl
+import urllib.request
+
+links = []
+with open('Products.json', encoding='utf-8') as f:
+    data = json.load(f)
+    for d in data:
+        ext = d['image'].split('/')[-1].split('.')[-1]
+        links.append({
+            'name': f'{d["_id"]["$oid"]}.{ext}',
+            'link': d['image']
+        })
+
+if not os.path.exists('images'):
+    os.mkdir('images')
+
+total_num = len(links)
+
+for idx, image in enumerate(links, start=1):
+    image_url = image['link']  # the image on the web
+    save_name = f'images/{image["name"]}'  # local name to be saved
+    urllib.request.urlretrieve(image_url, save_name)
+    print(f'{idx}/{total_num}')
